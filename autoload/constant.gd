bce6ad9d328830e4eab468104bbd0a3a178a7ed6
Steven Coghill <scoghill1955@gmail.com>
2024-04-17 10:09:17 -0400

    add click detect

--- autoload/constant.gd
+++ autoload/constant.gd
@@ -1,8 +1,10 @@
-class_name Constnt
+class_name Constant
 extends Node
 
 
 const PICTURE_WIDTH := 600
 const PICTURE_HEIGHT := 400
 const PATTERN_SIZE := 50
+const HFRAME_COUNT := 12
+const VFRAME_COUNT := 8
 
--- script/game_play.gd
+++ script/game_play.gd
@@ -18,11 +18,18 @@ extends Node2D
 
 # exports (The following properties must be set in the Inspector by the designer)
 
+@export var picture_area_vertical_offset := 60
+
 # public variables
 
 # private variables
 
+var box: Rect2
+
 # onready variables
+
+@onready var picture_area := $PictureArea
+@onready var picture := $PictureArea/Picture
 
 #endregion
 
@@ -37,25 +44,31 @@ extends Node2D
 # Return
 #		None
 #==
-# What the code is doing (steps)
-# NOTE: Child must call super._ready() if it defines own _ready() method
+# Place the picture in the play area
+# Create Rect2 of the picture
 func _ready() -> void:
-	pass
+	var vp = get_viewport_rect()
+	picture.position.x = vp.end.x / 2 - Constant.PICTURE_WIDTH / 2
+	picture.position.y = picture_area_vertical_offset
+	box = Rect2(picture.position, 
+			Vector2(Constant.PICTURE_WIDTH, Constant.PICTURE_HEIGHT))
 
-
-# _process(delta)
-# Called once per frame
+# _input(event)
+# Look for mouse clicks
 #
 # Parameters
-#	delta: float            	Seconds elapsed since last frame
+#	event: InputEvent          	Seconds elapsed since last frame
 # Return
 #	None
 #==
-# What the code is doing (steps)
-# NOTE: Child must call super._ready() if it defines own _ready() method
-func _process(delta) -> void:
-	pass
 
+func _input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.pressed): 
+		var frame := get_frame_clicked(event.position)
+		if frame >= 0:
+			print("Frame: ", frame)
 
 # Built-in Signal Callbacks
 
@@ -67,6 +80,32 @@ func _process(delta) -> void:
 
 
 # Private Methods
+
+# get_frame_clicked(pos)
+# Check if mouse clicked in the picture.
+# If so, then return what animation frame was clicked
+#
+# Parameters
+#	pos: Vector2					Mouse position at time of click
+# Return
+#	int								Frame number corresponding to click position
+#	-1								Click wasn't in the picture
+#==
+# Check if the position is in the box.
+# If not, then return -1
+# Calculate the frame number and return it
+func get_frame_clicked(pos: Vector2) -> int:
+	if not box.has_point(pos):
+		return -1
+		
+	var frame_number: int 	
+	var boxl = pos.x - box.position.x
+	var boxh = pos.y - box.position.y
+	var segx: int = boxl / Constant.PATTERN_SIZE
+	var segy: int = boxh / Constant.PATTERN_SIZE
+	frame_number = segx + (segy * 12)
+
+	return frame_number
 
 
 # Subclasses
